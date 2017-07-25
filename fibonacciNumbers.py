fcb6e83ca70f2057ad0754377a4638506e4d00c5
Thethosaurus <jorgebarrios547@gmail.com>
2017-07-24 21:53:51 -0500

    Add files via upload

---
+++ fibonacciNumbers.py
@@ -0,0 +1,15 @@
+
+userInput = int(raw_input("How many fibonacci numbers would you like to see? "))
+def fibonacciNumbers(n):
+    fibonacci = [1, 1]
+    nPLace = [1,2]
+    for i in range (1, n-2):
+        twoBackNumber = fibonacci[i-1]
+        oneBackNumber = fibonacci[i]
+        newNumber = twoBackNumber + oneBackNumber
+        fibonacci.append(newNumber)
+        nPLace.append(i+3)
+        i += 1
+    return fibonacci
+
+print fibonacciNumbers(userInput);
