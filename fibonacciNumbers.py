ae30fc86866bb92d5518c58f53be9a4838487584
Thethosaurus <jorgebarrios547@gmail.com>
2017-07-25 14:23:52 -0500

    Update fibonacciNumbers.py

--- fibonacciNumbers.py
+++ fibonacciNumbers.py
@@ -3,7 +3,7 @@ userInput = int(raw_input("How many fibonacci numbers would you like to see? "))
 def fibonacciNumbers(n):
     fibonacci = [1, 1]
     nPLace = [1,2]
-    for i in range (1, n-2):
+    for i in range (1, n-1):
         twoBackNumber = fibonacci[i-1]
         oneBackNumber = fibonacci[i]
         newNumber = twoBackNumber + oneBackNumber
